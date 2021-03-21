09e0b36ca5a6342bb0561856bff99015ebc72ef7
ookimaki <noreply@ookimaki.com>
2021-03-21 21:50:01 +0900

    fix front

--- packages/frontend/src/modules/web3.ts
+++ packages/frontend/src/modules/web3.ts
@@ -4,7 +4,7 @@ import Web3 from "web3";
 import Web3Modal from "web3modal";
 import { abi as lettersAbi } from "../../../contracts/artifacts/contracts/Letters.sol/Letters.json";
 import chainIdConfig from "../../../contracts/chainIds.json";
-import { NetworkName } from "../../../contracts/helpers/types";
+import { NetworkName, ChainId } from "../../../contracts/helpers/types";
 import networkConfig from "../../../contracts/networks.json";
 import { Letters } from "../../../contracts/typechain";
 
@@ -49,11 +49,11 @@ export const getWeb3 = async (provider: any) => {
   return new Web3(provider);
 };
 
-export const getNetworkNameFromChainId = (chainId: any): NetworkName => {
+export const getNetworkNameFromChainId = (chainId: ChainId): NetworkName => {
   return chainIdConfig[chainId] as NetworkName;
 };
 
-export const getContractsForChainId = (chainId: string) => {
+export const getContractsForChainId = (chainId: ChainId) => {
   const networkName = getNetworkNameFromChainId(chainId);
   const { letters, rpc, explore } = networkConfig[networkName];
   const provider = new ethers.providers.JsonRpcProvider(rpc);
